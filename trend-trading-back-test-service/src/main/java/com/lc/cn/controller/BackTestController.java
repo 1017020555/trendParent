7feddef6e3ac632394b49ccc101ca96647625f09
lc <1017020555@qq.com>
2020-06-03 18:00:07 +0800

    模拟回测-收益对比图

--- trend-trading-back-test-service/src/main/java/com/lc/cn/controller/BackTestController.java
+++ trend-trading-back-test-service/src/main/java/com/lc/cn/controller/BackTestController.java
@@ -3,27 +3,32 @@ package com.lc.cn.controller;
  * @Author: lichao
  * @Date: 2020/6/1 16:33
  */
+
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.StrUtil;
 import com.lc.cn.pojo.IndexData;
+import com.lc.cn.pojo.Profit;
 import com.lc.cn.service.BackTestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.*;
 
 @RestController
 public class BackTestController {
-    @Autowired BackTestService backTestService;
+    @Autowired
+    BackTestService backTestService;
 
     @GetMapping("/simulate/{code}/{startDate}/{endDate}")
     @CrossOrigin
     public Map<String,Object> backTest(
             @PathVariable("code") String code
             ,@PathVariable("startDate") String strStartDate
-            ,@PathVariable("endDate") String strEndDate) throws Exception {
+            ,@PathVariable("endDate") String strEndDate
+    ) throws Exception {
         List<IndexData> allIndexDatas = backTestService.listIndexData(code);
 
         String indexStartDate = allIndexDatas.get(0).getDate();
@@ -31,11 +36,18 @@ public class BackTestController {
 
         allIndexDatas = filterByDateRange(allIndexDatas,strStartDate, strEndDate);
 
+        int ma = 20;
+        float sellRate = 0.95f;
+        float buyRate = 1.05f;
+        float serviceCharge = 0f;
+        Map<String,?> simulateResult= backTestService.simulate(ma,sellRate, buyRate,serviceCharge, allIndexDatas);
+        List<Profit> profits = (List<Profit>) simulateResult.get("profits");
+
         Map<String,Object> result = new HashMap<>();
         result.put("indexDatas", allIndexDatas);
         result.put("indexStartDate", indexStartDate);
         result.put("indexEndDate", indexEndDate);
-
+        result.put("profits", profits);
         return result;
     }
 
